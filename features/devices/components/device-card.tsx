7c47ee0dd8d4cb47e98d38d784280dd061bf4473
Martin <mselvenis@gmail.com>
2024-11-29 17:13:45 +0000

    fix: split device name and badge between

--- features/devices/components/device-card.tsx
+++ features/devices/components/device-card.tsx
@@ -5,8 +5,10 @@ export const DeviceCard = (props: DeviceShemaType) => {
   return (
     <>
       <div className="flex flex-col space-y-2 p-4 border rounded-lg">
-        <span className="font-medium">{props.name}</span>
-        <Badge>{props.deviceType.name}</Badge>
+        <div className="flex justify-between items-center">
+          <span className="font-medium">{props.name}</span>
+          <Badge>{props.deviceType.name}</Badge>
+        </div>
         <div className="text-sm text-gray-500">
           <p>MAC: {props.mac}</p>
           <p>IP: {props.ip}</p>
